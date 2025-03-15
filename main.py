1238e50cbe5b2961ea764ed46d0c01580c339b54
Bhargav Chataut <bhargavchataut101@gmail.com>
2025-03-15 18:31:32 -0500

    Frontend Graph with dummy data

--- main.py
+++ main.py
@@ -13,3 +13,6 @@ y_axis = st.selectbox("Select the data for the Y-axis",
               "Life Expectancy","Freedom to make life choices",
               "Generosity","Corruption"))
 
+graph = px.scatter(x=[1,2], y=[1,2],
+                   labels={"x": x_axis, "y": y_axis})
+st.plotly_chart(graph)

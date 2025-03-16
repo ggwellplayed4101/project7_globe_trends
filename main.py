e39b11bdf2896b4f0a4df61c7d78c27d0dffa0ad
Bhargav Chataut <bhargavchataut101@gmail.com>
2025-03-15 19:09:35 -0500

    Country name made visible when hovering over a data point

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ y_axis = st.selectbox("Select the data for the Y-axis",
               "Life Expectancy","Freedom to make life choices",
               "Generosity","Corruption"))
 
+df = pd.read_csv("happy.csv")
 def get_data(topic):
-    df = pd.read_csv("happy.csv")
     data_dict = {
         "Happiness": "happiness",
         "GDP": "gdp",
@@ -28,9 +28,12 @@ def get_data(topic):
     data = df[data_dict[topic]]
     return(data)
 
+st.subheader(f"{x_axis} and {y_axis}")
+
 x_data = get_data(x_axis)
 y_data = get_data(y_axis)
 
 graph = px.scatter(x=x_data, y=y_data,
-                   labels={"x": x_axis, "y": y_axis})
+                   labels={"x": x_axis, "y": y_axis},
+                   hover_name=df["country"])
 st.plotly_chart(graph)

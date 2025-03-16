e2b8be28113779649130e3b5b45110efd4a98940
Bhargav Chataut <bhargavchataut101@gmail.com>
2025-03-15 19:01:23 -0500

    loaded data and made the graph responsive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+import pandas as pd
 
 st.title("Exploring Correlations Between Different National Metrics")
 
@@ -13,6 +14,23 @@ y_axis = st.selectbox("Select the data for the Y-axis",
               "Life Expectancy","Freedom to make life choices",
               "Generosity","Corruption"))
 
-graph = px.scatter(x=[1,2], y=[1,2],
+def get_data(topic):
+    df = pd.read_csv("happy.csv")
+    data_dict = {
+        "Happiness": "happiness",
+        "GDP": "gdp",
+        "Social Support": "social_support",
+        "Life Expectancy": "life_expectancy",
+        "Freedom to make life choices": "freedom_to_make_life_choices",
+        "Generosity": "generosity",
+        "Corruption": "corruption"
+    }
+    data = df[data_dict[topic]]
+    return(data)
+
+x_data = get_data(x_axis)
+y_data = get_data(y_axis)
+
+graph = px.scatter(x=x_data, y=y_data,
                    labels={"x": x_axis, "y": y_axis})
 st.plotly_chart(graph)
